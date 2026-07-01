dab46598cfe8fd310036b13ef0feeee4594b3796
assabbour <assabbour@hotmail.com>
2026-07-01 15:05:28 +0200

    feat(list): add artwork list screen and set it as the initial view

---
+++ ArtworkListView.swift
@@ -0,0 +1,8 @@
+//
+//  ArtworkListView.swift
+//  urban_canvas
+//
+//  Created by apprenant 108 on 01/07/2026.
+//
+
+import Foundation
---
+++ Views/ArtworkListView.swift
@@ -0,0 +1,23 @@
+
+import Foundation
+import SwiftUI
+
+// Écran qui affiche toutes les œuvres disponibles.
+struct ArtworkListView: View {
+
+    // Données statiques utilisées par l'écran.
+    let artworks = MockData.artworks
+
+    var body: some View {
+        NavigationStack {
+            List(artworks) { artwork in
+                ArtworkRowView(artwork: artwork)
+            }
+            .navigationTitle("Liste des Street arts")
+        }
+    }
+}
+
+#Preview {
+    ArtworkListView()
+}
