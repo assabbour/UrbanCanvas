154b93b1d7b0c535e960bca804e77da1de15b4f3
assabbour <assabbour@hotmail.com>
2026-07-01 13:11:34 +0200

    chore: initialize project and create base folder structure

---
+++ App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  urban_canvas
+//
+//  Created by apprenant 108 on 01/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ App/urban_canvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  urban_canvasApp.swift
+//  urban_canvas
+//
+//  Created by apprenant 108 on 01/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct urban_canvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
