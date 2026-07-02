89442d543f9f46f99acb6af2df6aa8d163c0cc3f
assabbour <assabbour@hotmail.com>
2026-07-02 13:52:09 +0200

    test(view): verify artwork detail view receives the correct artist

--- urban_canvasTests/Views/ArtworkDetailViewTests.swift
+++ urban_canvasTests/Views/ArtworkDetailViewTests.swift
@@ -12,4 +12,13 @@ struct ArtworkDetailViewTests {
 
         #expect(view.artwork.title == artwork.title)
     }
+    
+    
+    // Vérifie que la vue détail reçoit correctement l'artiste.
+    @Test("La vue détail reçoit le bon artiste")
+    func artworkDetailViewShouldReceiveArtist() {
+        let artwork = MockData.artworks[0]
+        let view = ArtworkDetailView(artwork: artwork)
+        #expect(view.artwork.artist.name == artwork.artist.name)
+    }
 }
