d2d37e6c0527af0205c1e0e4fd565a3fa4ff2a41
assabbour <assabbour@hotmail.com>
2026-07-02 12:15:42 +0200

    test(setup): initialize Swift Testing target and project structure

---
+++ urban_canvasTests/urban_canvasTests.swift
@@ -0,0 +1,18 @@
+//
+//  urban_canvasTests.swift
+//  urban_canvasTests
+//
+//  Created by apprenant 108 on 02/07/2026.
+//
+
+import Testing
+
+struct urban_canvasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
