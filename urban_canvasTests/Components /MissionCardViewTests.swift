b09cf0e863334089242c37b88a124255a4fbf51a
assabbour <assabbour@hotmail.com>
2026-07-06 09:49:22 +0200

    test(mission): add mission card creation test

---
+++ urban_canvasTests/Components /MissionCardViewTests.swift
@@ -0,0 +1,22 @@
+import Foundation
+import Testing
+@testable import urban_canvas
+
+struct MissionCardViewTests {
+
+    // Vérifie que la carte de mission peut être créée.
+    @Test("La carte Mission peut être créée")
+    func missionCardCanBeCreated() {
+
+        let item = MissionItem(
+            artwork: MockData.artworks[0]
+        )
+
+        let view = MissionCardView(
+            index: 1,
+            item: item
+        )
+
+        #expect(view.index == 1)
+    }
+}
